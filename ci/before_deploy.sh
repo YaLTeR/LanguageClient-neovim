f594cba5d1aefe5b50b4acc66c682c586cd31655
Junfeng Li <autozimu@gmail.com>
2017-12-11 20:55:51 -0800

    Fix deploy script.

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -29,8 +29,26 @@ package() {
     rm -rf $stage
 }
 
+release_tag() {
+    case $TRAVIS_OS_NAME in
+        linux)
+            cross build --target $TARGET --release
+            cp --force target/$TARGET/release/$CRATE_NAME bin/
+            ;;
+        osx)
+            make release
+            ;;
+    esac
+
+    git add --force bin/$CRATE_NAME
+    git commit --message "Add binary for $TRAVIS_TAG-$TARGET."
+    tagname="binary-$TRAVIS_TAG-$TARGET"
+    git tag --force "$tagname"
+    git push --force origin "$tagname"
+}
+
 TARGETS=(${TARGETS//:/ })
 for TARGET in "${TARGETS[@]}"; do
+    release_tag $TARGET
     package $TARGET
-    make release-tag
 done
